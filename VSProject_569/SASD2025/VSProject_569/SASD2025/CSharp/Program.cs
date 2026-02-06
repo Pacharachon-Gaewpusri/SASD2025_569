9fa90dcab2c4da5b5813c1f7bdf2e1e71b957926
Pacharachon-Gaewpusri <pacharachongaewpusri@gmail.com>
2026-02-06 16:37:06 +0700

    Add files via upload

---
+++ VSProject_569/SASD2025/VSProject_569/SASD2025/CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            new Examples().Run();
+        }
+
+
+    }
+}
